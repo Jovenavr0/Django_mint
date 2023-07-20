cc9dc8f009914f8c4598734447ef350740e0c234
Jovenavr0 <vvkarpenko2005@mail.ru>
2023-07-20 23:51:08 +0300

    worl with reser password

--- mint/users/forms.py
+++ mint/users/forms.py
@@ -1,12 +1,19 @@
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import get_user_model
 from django import forms
+from django.utils.translation import gettext_lazy as _
  
 User = get_user_model()
 
 class UserCreationForm(UserCreationForm):
     username = forms.CharField(label='Логин', widget=forms.TextInput(attrs={'class': 'form-control'}))
+    email = forms.EmailField(
+        label=_("Email"),
+        max_length=254,
+        widget=forms.EmailInput(attrs={"autocomplete": "email", 'class': 'form-control'}),
+    )
     password1 = forms.CharField(label='Пароль', widget=forms.PasswordInput(attrs={'class': 'form-control'}))
     password2 = forms.CharField(label='Повторите пароль', widget=forms.PasswordInput(attrs={'class': 'form-control'}))
     class Meta(UserCreationForm.Meta):
         model =  User
+        fields = ("username", "email")
